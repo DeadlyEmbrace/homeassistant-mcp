7b82c45640dc0c3572f4233b132b97114a35aa88
Zane Tait <zane.tait@silverfernfarms.co.nz>
2025-08-27 12:01:36 +1200

    fixes

--- src/config/hass.config.ts
+++ src/config/hass.config.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const HASS_CONFIG = {
-    BASE_URL: process.env.HASS_HOST || 'http://homeassistant.local:8123',
+    BASE_URL: process.env.HASS_HOST || 'http://192.168.3.148:8123',
     TOKEN: process.env.HASS_TOKEN || '',
-    SOCKET_URL: process.env.HASS_SOCKET_URL || '',
+    SOCKET_URL: process.env.HASS_SOCKET_URL || 'ws://192.168.3.148:8123/api/websocket',
     SOCKET_TOKEN: process.env.HASS_TOKEN || '',
 }; 
